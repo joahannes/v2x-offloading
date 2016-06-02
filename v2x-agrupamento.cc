9f264951d5bcc197c457c67bdfbf5286ee535a44
Joahannes Bruno <joahannes@gmail.com>
2016-06-02 00:27:50 -0300

    Transmissão para todos nós

--- v2x-agrupamento.cc
+++ v2x-agrupamento.cc
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2015 University of Athens (UOA)
+ * Copyright (c) 2016
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author:  - Joahannes Costa <joahannes@gmail.com>
+ * Author: Joahannes Costa <joahannes@gmail.com>
  * 
  */
 
@@ -218,8 +218,8 @@ int main(int argc, char *argv[]) {
     double start = 7.0;
     double stop = simTime; 
     
-    uint16_t port = 4000;
-    uint16_t m_port = port*i;
+    uint16_t port = 2000;
+    uint16_t m_port = port * i + 2000; //Para alcançar o nó ZERO quando i = 0
 
     EvalvidServerHelper server (m_port);
      server.SetAttribute ("SenderTraceFilename", StringValue("video/st_a03"));
